25253d92314d5e9f159347331ba185d8a5fd737e
Gowthami-76 <Gowthamithapa77@gmail.com>
2024-07-26 14:26:17 +0530

    enable cors changed

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
-    origin: [
-      'http://localhost:5174',
-    ],
+    origin: ['http://localhost:5174', 'http://localhost:5174'],
   });
 
   app.use(express.static(join(process.cwd(), './public/')));
